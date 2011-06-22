41631bf968c0b9d94564baec9abf4e49c6590847
Pekka Nikander <pekka.nikander@senseg.com>
2011-06-22 16:44:09 +0300

    Initial Android port.

---
+++ Android.mk
@@ -0,0 +1,25 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := iconv
+
+LOCAL_CFLAGS    := \
+	-DHAVE_CONFIG_H -DBUILDING_LIBICONV -DBUILDING_DLL \
+	-DENABLE_RELOCATABLE=1 -DIN_LIBRARY -DNO_XMALLOC
+	-Wno-multichar \
+	-DLIBDIR="\"c\"" \
+
+LOCAL_C_INCLUDES := \
+	$(LOCAL_PATH) \
+	$(LOCAL_PATH)/include \
+	$(LOCAL_PATH)/lib \
+	$(LOCAL_PATH)/libcharset/include
+
+LOCAL_SRC_FILES := \
+	lib/iconv.c \
+	libcharset/lib/localcharset.c \
+	lib/relocatable.c 
+
+include $(BUILD_STATIC_LIBRARY)
+
---
+++ config.h
@@ -0,0 +1,10 @@
+#ifndef _CONFIG_H
+#define _CONFIG_H 1
+
+/* Definitions for iconv */
+
+#define HAVE_WORKING_O_NOFOLLOW 0
+#define LIBDIR "c"
+#define ICONV_CONST 
+
+#endif /* _CONFIG_H */
