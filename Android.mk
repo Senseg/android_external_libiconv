78ebbbadc6c0ce5cd12e6fe1e90e9efeafdaee28
Pekka Nikander <pekka.nikander@senseg.com>
2011-06-23 15:42:43 +0300

    Rename the module to libiconv

--- Android.mk
+++ Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE    := iconv
+LOCAL_MODULE    := libiconv
 
 LOCAL_CFLAGS    := \
 	-DHAVE_CONFIG_H -DBUILDING_LIBICONV -DBUILDING_DLL \
